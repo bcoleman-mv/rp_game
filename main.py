58d7d07e40d713101be8185b6b57959a96865ad1
Bralin Coleman <bcoleman.dev@gmail.com>
2023-07-08 19:58:20 -0400

    updated player class

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ player_pos = pygame.Vector2(WIN.get_width() / 2, WIN.get_height() / 2)
 player1 = Player("red", 25)
 
 
-def draw_window(color: tuple[int,int,int]):
+def draw_window(color: tuple[int,int,int], player: Player):
     WIN.fill(color)
+    draw_player(player)
     pygame.display.update()
 
 def draw_player(player: Player):
@@ -26,19 +27,38 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         
-        draw_window(WHITE)
-        draw_player(player1)
+        if player1.position.x >= WIDTH:
+            player1.change_direction("left")
+        elif player1.position.x <= 0:
+            player1.change_direction("right")
+        elif player1.position.y >= HEIGHT:
+            player1.change_direction("up")
+        elif player1.position.y <= 0:
+            player1.change_direction("down")
+            
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            player1.change_direction("up")
+        if keys[pygame.K_s]:
+            player1.change_direction("down")
+        if keys[pygame.K_a]:
+            player1.change_direction("left")
+        if keys[pygame.K_d]:
+            player1.change_direction("right")
         
-        if KEYS[pygame.K_w]:
-            player1.move(pygame.K_w)
-        if KEYS[pygame.K_s]:
-            player1.move(pygame.K_s)
-        if KEYS[pygame.K_a]:
-            player1.move(pygame.K_a)
-        if KEYS[pygame.K_d]:
-            player1.move(pygame.K_d)
+        player1.move()
+        draw_window(WHITE, player1)
+        
+        # if KEYS[pygame.K_w]:
+        #     player1.move(pygame.K_w)
+        # if KEYS[pygame.K_s]:
+        #     player1.move(pygame.K_s)
+        # if KEYS[pygame.K_a]:
+        #     player1.move(pygame.K_a)
+        # if KEYS[pygame.K_d]:
+        #     player1.move(pygame.K_d)
 
-        pygame.display.flip()
+        # pygame.display.flip()
         # dt = CLOCK.tick(60) / 1000
         
     print("Game Ended")
--- player.py
+++ player.py
@@ -4,21 +4,65 @@ class Circle:
     def __init__(self, color:str, size:int):
         self.color = color
         self.size = size
-
+    
 class Player(Circle):
-    def __init__(self, color: str, size: int, position: pygame.Vector2 = None):
+    def __init__(self, color: str, size: int, position: pygame.Vector2 = None, direction:str = None):
         super().__init__(color, size)
         if position == None:
             self.position = pygame.Vector2(WIN.get_width() / 2, WIN.get_height() / 2)
         else:
             self.position = position
+            
+        if direction == None:
+            self.direction = "right"
+        else:
+            self.direction = direction
         
-    def move(self, key:int):
-        if key==pygame.K_w:
-            self.position.y -= PLAYER_MOVE_DISTANCE
-        if key==pygame.K_s:
-            self.position.y += PLAYER_MOVE_DISTANCE
-        if key==pygame.K_a:
-            self.position.x -= PLAYER_MOVE_DISTANCE
-        if key==pygame.K_d:
-            self.position.x += PLAYER_MOVE_DISTANCE
+    # def move(self, key:int):
+    #     if key==pygame.K_w:
+    #         self.position.y -= PLAYER_MOVE_DISTANCE
+    #     if key==pygame.K_s:
+    #         self.position.y += PLAYER_MOVE_DISTANCE
+    #     if key==pygame.K_a:
+    #         self.position.x -= PLAYER_MOVE_DISTANCE
+    #     if key==pygame.K_d:
+    #         self.position.x += PLAYER_MOVE_DISTANCE
+    #     print(f"Player moved: {self.position.x}, {self.position.y}")
+    
+    def move_right(self):
+        # self.change_direction("right")
+        self.position.x += 1
+        print("Moved right")
+        
+    def move_left(self):
+        # self.change_direction("left")
+        self.position.x -= 1
+        print("Moved left")
+        
+    def move_up(self):
+        # self.change_direction("up")
+        self.position.y -= 1
+        print("Moved up")
+        
+    def move_down(self):
+        # self.change_direction("down")
+        self.position.y += 1
+        print("Moved down")
+        
+    def move(self):
+        if self.direction == "left":
+            self.move_left()
+        elif self.direction == "right":
+            self.move_right()
+        elif self.direction == "up":
+            self.move_up()
+        elif self.direction == "down":
+            self.move_down()
+        print(f"Position: {self.position}")
+            
+    def change_direction(self, direction:str):
+        if direction in ["left", "right", "up", "down"]:
+            self.direction = direction
+        else:
+            raise ValueError(f"{self.direction} is not a valid direction")
+            
